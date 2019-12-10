d3d277a22c182632de4ce91a95663be08437643c
Jaehee Ko <celestarry@gmail.com>
2019-12-10 11:07:20 +0900

    Update to 1.002

--- BuzzvilSDK.framework/Headers/BuzzOfferwall.h
+++ BuzzvilSDK.framework/Headers/BuzzOfferwall.h
@@ -6,14 +6,16 @@
 //  Copyright © 2017년 Buzzvil. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 @interface BuzzOfferwall : NSObject
 
 + (BOOL)showOfferWallWithTitle:(NSString *)title;
 + (BOOL)showOfferWallWithTitle:(NSString *)title statusBarHidden:(BOOL)statusBarHidden;
++ (BOOL)showOfferWallWithTitle:(NSString *)title statusBarHidden:(BOOL)statusBarHidden presentationStyle:(UIModalPresentationStyle)presentationStyle;
 
 + (BOOL)showInquiryPage;
 + (BOOL)showInquiryPageWithStatusBarHidden:(BOOL)statusBarHidden;
++ (BOOL)showInquiryPageWithStatusBarHidden:(BOOL)statusBarHidden presentationStyle:(UIModalPresentationStyle)presentationStyle;
 
 @end
